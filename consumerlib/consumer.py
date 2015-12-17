94078c5ee1416406294df3d45bc1e1c5a00cede6
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2015-12-17 16:44:23 +0100

    Add optional setup subcommand to consumer

--- consumerlib/consumer.py
+++ consumerlib/consumer.py
@@ -8,8 +8,23 @@ from consumerlib import (init_safe_message_handler, loop, setup_consumer,
                          setup_consumer_client, TimeoutMessage)
 
 
-def initialize_timeout_consumer(url, name, on_message, on_final_death=None):
-    @click.command()
+def initialize_timeout_consumer(url, name, on_message, on_final_death=None,
+                                on_setup=None):
+
+    @click.group()
+    def _cli():
+        pass
+
+    if on_setup is not None:
+        @click.command('setup',
+                      help='setup consumer\'s queues and bindings')
+        def _consumer_setup():
+            consumer_client = setup_consumer_client(url)
+            on_setup(consumer_client)
+            exit()
+        _cli.add_command(_consumer_setup)
+
+    @click.command('run', help='run the consumer')
     @click.option('--process-number', type=int, default=0,
                   help='how many of these consumers were started')
     @click.option('--message-ttl', type=int,
@@ -17,8 +32,9 @@ def initialize_timeout_consumer(url, name, on_message, on_final_death=None):
     @click.option('--max-deaths', type=int,
                   help='number of tries a message can live through before '
                        'landing in the failed messages queue')
-    def consumer_runner(process_number, message_ttl, max_deaths):
+    def _consumer_runner(process_number, message_ttl, max_deaths):
         consumer_tag = '{}:{}'.format(name, process_number)
+
         on_error = TimeoutMessage(message_ttl, max_deaths,
                                   on_final_death=on_final_death)
 
@@ -32,4 +48,5 @@ def initialize_timeout_consumer(url, name, on_message, on_final_death=None):
 
         setup_consumer(consumer_client, name, consumer_tag, on_message_safe)
         loop([consumer_client])
-    return consumer_runner
+    _cli.add_command(_consumer_runner)
+    return _cli
